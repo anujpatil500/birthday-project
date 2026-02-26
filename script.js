9ce64bb2c928e9c57661eb581cd137eb95d706b6
anujpatil500 <101787713+anujpatil500@users.noreply.github.com>
2026-02-26 21:09:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -397,7 +397,10 @@ let draggedElement = null;
 const puzzleImages = [
     "images/1.jpg",
     "images/2.jpg",
-    "images/3.jpg"
+    "images/3.jpg",
+    "images/4.jpg",
+    "images/5.jpg",
+    "images/6.jpg"
 ];
 
 function initializePuzzleGame() {
@@ -713,6 +716,7 @@ function updateMessage(days, hours, minutes, seconds) {
 
 
 document.addEventListener("DOMContentLoaded", initializeCountdown);
+
 
 
 
